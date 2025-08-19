6aee93fafa82e9c263ae9c3ece2c294e4d3b3d1a
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-19 17:15:13 +0800

    Add the test file for the first binary operator

---
+++ src/kp_onnx_test/kop_sub_test.py
@@ -0,0 +1,23 @@
+from kp import Manager
+import numpy as np
+import time
+from kp_onnx.kop_sub import SubOp
+
+device_id = 1
+mgr = Manager(device_id)
+print(mgr.get_device_properties())
+
+numpy_in_1 = np.random.random((1023, 15))
+numpy_in_2 = np.random.random((3, 1023, 1023, 1))
+
+start_time = time.time()
+numpy_out = numpy_in_1 - numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+sub_op = SubOp(mgr, ['input1', 'input2'], ['output'])
+start_time = time.time()
+kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
+# print("Numpy:", numpy_out)
+# print("Kompute:", kp_out)
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
