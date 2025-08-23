2157712cae16793b5f04b8689b10a4cbe716d637
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-23 19:27:36 +0800

    Update kop_sub_test.py for covering the test of all branches

--- src/kp_onnx_test/kop_sub_test.py
+++ src/kp_onnx_test/kop_sub_test.py
@@ -7,7 +7,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-# case 1
+print('Case 1')
 numpy_in_1 = np.random.random((1023, 15))
 numpy_in_2 = np.random.random((3, 1023, 1023, 1))
 
@@ -21,7 +21,7 @@ kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
 print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
 
-# case 2
+print('Case 2')
 numpy_in_1 = np.random.random((3, 1023, 1023, 1))
 numpy_in_2 = np.random.random((1023, 15))
 
@@ -35,7 +35,35 @@ kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
 print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
 
-# case 3
+print('Case 3')
+numpy_in_1 = np.random.random((255, 255, 15))
+numpy_in_2 = np.random.random((3, 3, 255, 255, 1))
+
+start_time = time.time()
+numpy_out = numpy_in_1 - numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+sub_op = SubOp(mgr, ['input1', 'input2'], ['output'])
+start_time = time.time()
+kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 4')
+numpy_in_1 = np.random.random((3, 3, 255, 255, 1))
+numpy_in_2 = np.random.random((255, 255, 15))
+
+start_time = time.time()
+numpy_out = numpy_in_1 - numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+sub_op = SubOp(mgr, ['input1', 'input2'], ['output'])
+start_time = time.time()
+kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 5')
 numpy_in_1 = np.random.random((1023, 15))
 numpy_in_2 = np.random.random((1023, 1))
 
@@ -49,7 +77,7 @@ kp_out = sub_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{sub_op}:", kp_out.shape, time.time() - start_time, "seconds")
 print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
 
-# case 4
+print('Case 6')
 numpy_in_1 = np.random.random((1023,))
 numpy_in_2 = np.random.random((1,))
 
