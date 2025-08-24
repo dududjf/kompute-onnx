29d1f735d522314b37b38d26bec21f7242925fcf
fzc04 <3247348249@qq.com>
2025-08-24 14:22:08 +0800

    Update kop_acosh_test.py

--- src/kp_onnx_test/kop_acosh_test.py
+++ src/kp_onnx_test/kop_acosh_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-acosh_op = AcoshOp(mgr, ['input'], ['output'])
+acosh_op = AcoshOp(mgr)
 
 numpy_in = np.random.uniform(1, 1000, 1024 * 1024 * 16).astype(np.float32)
 
@@ -20,7 +20,4 @@ start_time = time.time()
 kp_out = acosh_op.run(numpy_in)[0]
 print(f"{acosh_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
-
