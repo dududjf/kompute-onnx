94b779e669a9378cd460b4ed18350ca66367e6c6
fzc04 <3247348249@qq.com>
2025-08-24 14:25:31 +0800

    Update kop_tanh_test.py

--- src/kp_onnx_test/kop_tanh_test.py
+++ src/kp_onnx_test/kop_tanh_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-tanh_op = TanhOp(mgr, ['input'], ['output'])
+tanh_op = TanhOp(mgr)
 
 numpy_in = np.random.uniform(-1000, 1000, 1024 * 1024 * 16)
 
@@ -20,6 +20,4 @@ start_time = time.time()
 kp_out = tanh_op.run(numpy_in)[0]
 print(f"{tanh_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
