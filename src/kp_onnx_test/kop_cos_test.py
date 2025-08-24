2843f3627d5112a0c0039990ada6b5963f2dde73
fzc04 <3247348249@qq.com>
2025-08-24 14:23:16 +0800

    Update kop_cos_test.py

--- src/kp_onnx_test/kop_cos_test.py
+++ src/kp_onnx_test/kop_cos_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-cos_op = CosOp(mgr, ['input'], ['output'])
+cos_op = CosOp(mgr)
 
 numpy_in = np.random.uniform(-100, 100, 1024 * 1024 * 16).astype(np.float32)
 
@@ -20,6 +20,4 @@ start_time = time.time()
 kp_out = cos_op.run(numpy_in)[0]
 print(f"{cos_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
