28e5c222d5f459556d6eb4b37e5001484eb543c7
fzc04 <3247348249@qq.com>
2025-08-24 14:22:34 +0800

    Update kop_atan_test.py

--- src/kp_onnx_test/kop_atan_test.py
+++ src/kp_onnx_test/kop_atan_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-atan_op = AtanOp(mgr, ['input'], ['output'])
+atan_op = AtanOp(mgr)
 
 numpy_in = np.random.uniform(-1000, 1000, 1024 * 1024 * 16).astype(np.float32)
 
@@ -20,6 +20,4 @@ start_time = time.time()
 kp_out = atan_op.run(numpy_in)[0]
 print(f"{atan_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
