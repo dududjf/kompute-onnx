ef949c6a2432e43a28f9e2040c7629f2d6588c65
fzc04 <3247348249@qq.com>
2025-08-24 14:21:39 +0800

    Update kop_abs_test.py

--- src/kp_onnx_test/kop_abs_test.py
+++ src/kp_onnx_test/kop_abs_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-abs_op = AbsOp(mgr, ['input'], ['output'])
+abs_op = AbsOp(mgr)
 
 numpy_in = np.random.uniform(-1000, 1000, 1024 * 1024 * 16).astype(np.float32)
 
@@ -20,6 +20,4 @@ start_time = time.time()
 kp_out = abs_op.run(numpy_in)[0]
 print(f"{abs_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
