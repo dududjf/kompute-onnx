1ee7c89be20e74c39b88bee4cba30421e56c2fac
fzc04 <3247348249@qq.com>
2025-08-24 14:24:44 +0800

    Update kop_sinh_test.py

--- src/kp_onnx_test/kop_sinh_test.py
+++ src/kp_onnx_test/kop_sinh_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-sinh_op = SinhOp(mgr, ['input'], ['output'])
+sinh_op = SinhOp(mgr)
 
 numpy_in = np.random.uniform(-80, 80, 1024 * 1024 * 16).astype(np.float32)
 
@@ -20,6 +20,4 @@ start_time = time.time()
 kp_out = sinh_op.run(numpy_in)[0]
 print(f"{sinh_op}:", time.time() - start_time, "seconds")
 
-print("numpy_out:", numpy_out)
-print("kp_out:", kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
