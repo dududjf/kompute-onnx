cf640fbaee1bd2f177bd83211310229f7d23100c
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-24 11:32:33 +0800

    Update kop_erf_test.py for initializing operators

--- src/kp_onnx_test/kop_erf_test.py
+++ src/kp_onnx_test/kop_erf_test.py
@@ -8,7 +8,7 @@ device_id = 0
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-erf_op = ErfOp(mgr, ['input'], ['output'])
+erf_op = ErfOp(mgr)
 vec_erf_func = np.vectorize(erf)
 numpy_in = np.random.random(1024 * 1024)
 
@@ -20,7 +20,4 @@ start_time = time.time()
 kp_out = erf_op.run(numpy_in)[0]
 print(f"{erf_op}:", time.time() - start_time, "seconds")
 
-print(numpy_out)
-print(kp_out)
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
-
