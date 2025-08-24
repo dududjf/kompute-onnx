b1fda502eb4fb310bcb73c057e63420fc8cb525f
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-24 11:33:27 +0800

    Update kop_matmul_test.py for initializing operators

--- src/kp_onnx_test/kop_matmul_test.py
+++ src/kp_onnx_test/kop_matmul_test.py
@@ -7,7 +7,7 @@ device_id = 1
 mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
-matmul_op = MatMulOp(mgr, ['input1', 'input2'], ['output'])
+matmul_op = MatMulOp(mgr)
 numpy_in_1 = np.random.random((2, 5, 1000, 512))
 numpy_in_2 = np.random.random((512, 1024))
 
@@ -19,8 +19,6 @@ start_time = time.time()
 kp_out = matmul_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{matmul_op}:", time.time() - start_time, "seconds")
 
-# print(numpy_out)
-# print(kp_out)
 print('Max error:', np.abs(numpy_out - kp_out).max())
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
 
@@ -35,8 +33,5 @@ start_time = time.time()
 kp_out = matmul_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{matmul_op}:", time.time() - start_time, "seconds")
 
-# print(numpy_out)
-# print(kp_out)
 print('Max error:', np.abs(numpy_out - kp_out).max())
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
-
