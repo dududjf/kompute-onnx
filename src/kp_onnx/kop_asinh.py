5e29097efcb6374a6274ba4fae585c6a20095498
fzc04 <3247348249@qq.com>
2025-08-26 14:57:19 +0800

    Create kop_asinh.py

---
+++ src/kp_onnx/kop_asinh.py
@@ -0,0 +1,54 @@
+import kp
+import numpy as np
+from pyshader import python2shader, ivec2, f32, Array
+from pyshader.stdlib import asinh
+
+
+@python2shader
+def compute_shader_asinh(index=("input", "GlobalInvocationId", ivec2),
+                         in_data=("buffer", 0, Array(f32)),
+                         out_data=("buffer", 1, Array(f32))):
+    i = index.x
+    out_data[i] = asinh(in_data[i])
+
+
+_asinh_code = compute_shader_asinh.to_spirv()
+
+
+class AsinhOp:
+    def __init__(self, manager: kp.Manager):
+        self.manager = manager
+
+    def __repr__(self):
+        device_name = self.manager.get_device_properties()['device_name']
+        return f"AsinhOp({device_name})"
+
+    def __str__(self):
+        device_name = self.manager.get_device_properties()['device_name']
+        return f"AsinhOp({device_name})"
+
+    def run(self, *inputs):
+        tensor_shape = inputs[0].shape
+        numpy_in = inputs[0].reshape(-1).astype(np.float32)
+        tensor_in = self.manager.tensor(numpy_in)
+        tensor_out = self.manager.tensor(np.zeros_like(numpy_in))
+        algo = self.manager.algorithm([tensor_in, tensor_out], _asinh_code)
+        seq = self.manager.sequence()
+        seq.record(kp.OpTensorSyncDevice([tensor_in])) \
+           .record(kp.OpAlgoDispatch(algo)) \
+           .record(kp.OpTensorSyncLocal([tensor_out])) \
+           .eval()
+        outputs = [tensor_out.data().reshape(tensor_shape)]
+        del tensor_in
+        del tensor_out
+        return outputs
+
+    def fuse(self, input_tensors: list[tuple[kp.Tensor, list[int]]], updated_algorithms: list[kp.Algorithm],
+             updated_tensors: list[kp.Tensor]) -> list[tuple[kp.Tensor, list[int]]]:
+        tensor_in = input_tensors[0][0]
+        tensor_shape = input_tensors[0][1]
+        size = np.prod(tensor_shape)
+        tensor_out = self.manager.tensor(np.zeros(size, dtype=np.float32))
+        updated_tensors.append(tensor_out)
+        updated_algorithms.append(self.manager.algorithm([tensor_in, tensor_out], _asinh_code))
+        return [(tensor_out, tensor_shape)]
