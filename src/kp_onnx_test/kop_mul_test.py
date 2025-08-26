8021e7ed56a391d8c77823f900c9ad1231a32b2f
fzc04 <3247348249@qq.com>
2025-08-26 15:02:42 +0800

    Create kop_mul_test.py

---
+++ src/kp_onnx_test/kop_mul_test.py
@@ -0,0 +1,87 @@
+from kp import Manager
+import numpy as np
+import time
+from kp_onnx.kop_mul import MulOp
+
+device_id = 0
+mgr = Manager(device_id)
+print(mgr.get_device_properties())
+mul_op = MulOp(mgr)
+
+print('Case 1')
+numpy_in_1 = np.random.random((1023, 15))
+numpy_in_2 = np.random.random((3, 1023, 1023, 1))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 2')
+numpy_in_1 = np.random.random((3, 1023, 1023, 1))
+numpy_in_2 = np.random.random((1023, 15))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 3')
+numpy_in_1 = np.random.random((255, 255, 15))
+numpy_in_2 = np.random.random((3, 3, 255, 255, 1))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 4')
+numpy_in_1 = np.random.random((3, 3, 255, 255, 1))
+numpy_in_2 = np.random.random((255, 255, 15))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 5')
+numpy_in_1 = np.random.random((1023, 15))
+numpy_in_2 = np.random.random((1023, 1))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
+
+print('Case 6')
+numpy_in_1 = np.random.random((1023,))
+numpy_in_2 = np.random.random((1,))
+
+start_time = time.time()
+numpy_out = numpy_in_1 * numpy_in_2
+print("Numpy:", numpy_out.shape, time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = mul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{mul_op}:", kp_out.shape, time.time() - start_time, "seconds")
+print(np.allclose(numpy_out, kp_out, rtol=1e-5, atol=1e-5))
