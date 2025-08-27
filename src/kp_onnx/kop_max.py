c720e61e3b02cd470a9a9aa8a57e026ce1ece77d
fzc04 <3247348249@qq.com>
2025-08-27 18:56:05 +0800

    Update kop_max.py

--- src/kp_onnx/kop_max.py
+++ src/kp_onnx/kop_max.py
@@ -16,7 +16,7 @@ layout (binding = 2) buffer buf_out_tensor { float out_tensor[]; };
 layout (constant_id = 0) const float size_x_1f = 0;
 layout (constant_id = 1) const float size_y_1f = 0;
 layout (constant_id = 2) const float size_z_1f = 0;
-layout (constant_id = 3) const float size_x_2f = 0;
+layout (constant_id = 3) const float size_x_2f = 0; 
 layout (constant_id = 4) const float size_y_2f = 0;
 layout (constant_id = 5) const float size_z_2f = 0;
 
@@ -45,9 +45,7 @@ void main()
     uint z_2 = min(gz, size_z_2 - 1);
     uint p_1 = x_1 * stride_x_1 + y_1 * stride_y_1 + z_1;
     uint p_2 = x_2 * stride_x_2 + y_2 * stride_y_2 + z_2;
-    float v1 = in_tensor_1[p_1];
-    float v2 = in_tensor_2[p_2];
-    out_tensor[gx * stride_x + gy * stride_y + gz] = (v1 > v2) ? v1 : v2;
+    out_tensor[gx * stride_x + gy * stride_y + gz] = max(in_tensor_1[p_1], in_tensor_2[p_2]);
 }''')
 
     def __repr__(self):
@@ -59,168 +57,120 @@ void main()
         return f"MaxOp({device_name})"
 
     def run(self, *inputs):
-        assert len(inputs) == 2, "MaxOp requires 2 inputs"
-        input_1 = inputs[0]
-        input_2 = inputs[1]
-        if input_1.ndim < input_2.ndim:
-            new_shape_1 = [1] * (input_2.ndim - input_1.ndim) + list(input_1.shape)
-            new_shape_2 = list(input_2.shape)
-        elif input_2.ndim < input_1.ndim:
-            new_shape_1 = list(input_1.shape)
-            new_shape_2 = [1] * (input_1.ndim - input_2.ndim) + list(input_2.shape)
-        else:
-            new_shape_1 = list(input_1.shape)
-            new_shape_2 = list(input_2.shape)
-        output_shape = []
-        for i in range(len(new_shape_1)):
-            if new_shape_1[i] == 1:
-                output_shape.append(new_shape_2[i])
-            elif new_shape_2[i] == 1:
-                output_shape.append(new_shape_1[i])
-            else:
-                assert new_shape_1[i] == new_shape_2[i], f"MaxOp requires input {i} of the same shape"
-                output_shape.append(new_shape_1[i])
+        if len(inputs) == 1:
+            output = inputs[0].astype(np.float32, copy=True)
+            return [output]
 
-        numpy_in_1 = input_1.reshape(-1).astype(np.float32)
-        tensor_in_1 = self.manager.tensor(numpy_in_1)
-        new_in_1 = tensor_in_1
-        algorithms_1, next_tensors_1 = [], []
-        if output_shape[:-2] != new_shape_1[:-2] and not all(e == 1 for e in new_shape_1[:-2]):
-            final_shape_1 = output_shape[:-2] + list(new_shape_1[-2:])
-            new_in_1 = broadcast_to(tensor_in_1, new_shape_1, final_shape_1, algorithms_1, next_tensors_1, self.manager)
-            new_shape_1 = final_shape_1
+        input_tensors = []
+        for inp in inputs:
+            numpy_in = inp.reshape(-1).astype(np.float32)
+            tensor = self.manager.tensor(numpy_in)
+            input_tensors.append((tensor, list(inp.shape)))
 
-        numpy_in_2 = input_2.reshape(-1).astype(np.float32)
-        tensor_in_2 = self.manager.tensor(numpy_in_2)
-        new_in_2 = tensor_in_2
-        algorithms_2, next_tensors_2 = [], []
-        if output_shape[:-2] != new_shape_2[:-2] and not all(e == 1 for e in new_shape_2[:-2]):
-            final_shape_2 = output_shape[:-2] + list(new_shape_2[-2:])
-            new_in_2 = broadcast_to(tensor_in_2, new_shape_2, final_shape_2, algorithms_2, next_tensors_2, self.manager)
-            new_shape_2 = final_shape_2
-
-        if len(new_shape_1) == 1:
-            size_x_1 = new_shape_1[0]
-            size_y_1 = 1
-            size_z_1 = 1
-            size_x_2 = new_shape_2[0]
-            size_y_2 = 1
-            size_z_2 = 1
-        elif len(new_shape_1) == 2:
-            size_x_1 = new_shape_1[0]
-            size_y_1 = new_shape_1[1]
-            size_z_1 = 1
-            size_x_2 = new_shape_2[0]
-            size_y_2 = new_shape_2[1]
-            size_z_2 = 1
-        else:
-            size_x_1 = np.prod(new_shape_1[:-2])
-            size_y_1 = new_shape_1[-2]
-            size_z_1 = new_shape_1[-1]
-            size_x_2 = np.prod(new_shape_2[:-2])
-            size_y_2 = new_shape_2[-2]
-            size_z_2 = new_shape_2[-1]
-
-        size = np.prod(output_shape)
-        tensor_out = self.manager.tensor(np.zeros(size, dtype=np.float32))
-        workgroup = (max(size_x_1, size_x_2), max(size_y_1, size_y_2), max(size_z_1, size_z_2))
-        algo = self.manager.algorithm([new_in_1, new_in_2, tensor_out],
-                                      self.compiled_shader,
-                                      workgroup,
-                                      [size_x_1, size_y_1, size_z_1, size_x_2, size_y_2, size_z_2],
-                                      [])
+        updated_algorithms, updated_tensors = [], []
+        output_tensor_and_shape = self.fuse(input_tensors, updated_algorithms, updated_tensors)
+        tensor_out, output_shape = output_tensor_and_shape[0]
 
         seq = self.manager.sequence()
-        seq.record(kp.OpTensorSyncDevice([tensor_in_1, tensor_in_2]))
-        for alg_1 in algorithms_1:
-            seq.record(kp.OpAlgoDispatch(alg_1))
-        for alg_2 in algorithms_2:
-            seq.record(kp.OpAlgoDispatch(alg_2))
-        seq.record(kp.OpAlgoDispatch(algo))
+        seq.record(kp.OpTensorSyncDevice([t[0] for t in input_tensors]))
+        for alg in updated_algorithms:
+            seq.record(kp.OpAlgoDispatch(alg))
         seq.record(kp.OpTensorSyncLocal([tensor_out]))
         seq.eval()
-        output = tensor_out.data()
-        outputs = [output.reshape(output_shape)]
+
+        output = tensor_out.data().reshape(output_shape)
+
+        tensor_in_1, _ = input_tensors[0]
+        tensor_in_2, _ = input_tensors[1] if len(input_tensors) > 1 else (None, None)
+        next_tensors_1 = []
+        next_tensors_2 = []
 
         del tensor_in_1
         del next_tensors_1
         del tensor_in_2
         del next_tensors_2
         del tensor_out
-        return outputs
+
+        return [output]
 
     def fuse(self, input_tensors: list[tuple[kp.Tensor, list[int]]], updated_algorithms: list[kp.Algorithm],
              updated_tensors: list[kp.Tensor]) -> list[tuple[kp.Tensor, list[int]]]:
-        assert len(input_tensors) == 2, "MaxOp requires 2 inputs"
-        input_1 = input_tensors[0][0]
-        input_2 = input_tensors[1][0]
-        shape_1 = input_tensors[0][1]
-        shape_2 = input_tensors[1][1]
-        if len(shape_1) < len(shape_2):
-            new_shape_1 = [1] * (len(shape_2) - len(shape_1)) + shape_1
-            new_shape_2 = shape_2
-        elif len(shape_2) < len(shape_1):
-            new_shape_1 = shape_1
-            new_shape_2 = [1] * (len(shape_1) - len(shape_2)) + shape_2
-        else:
-            new_shape_1 = shape_1
-            new_shape_2 = shape_2
-        output_shape = []
-        for i in range(len(new_shape_1)):
-            if new_shape_1[i] == 1:
-                output_shape.append(new_shape_2[i])
-            elif new_shape_2[i] == 1:
-                output_shape.append(new_shape_1[i])
+
+        if len(input_tensors) == 1:
+            return input_tensors
+
+        def _fuse_two(f2_input):
+            input_1, shape_1 = f2_input[0]
+            input_2, shape_2 = f2_input[1]
+
+            if len(shape_1) < len(shape_2):
+                new_shape_1 = [1] * (len(shape_2) - len(shape_1)) + list(shape_1)
+                new_shape_2 = list(shape_2)
+            elif len(shape_2) < len(shape_1):
+                new_shape_1 = list(shape_1)
+                new_shape_2 = [1] * (len(shape_1) - len(shape_2)) + list(shape_2)
             else:
-                assert new_shape_1[i] == new_shape_2[i], f"MaxOp requires input {i} of the same shape"
-                output_shape.append(new_shape_1[i])
+                new_shape_1, new_shape_2 = list(shape_1), list(shape_2)
 
-        new_in_1 = input_1
-        algorithms_1, next_tensors_1 = [], []
-        if output_shape[:-2] != new_shape_1[:-2] and not all(e == 1 for e in new_shape_1[:-2]):
-            final_shape_1 = output_shape[:-2] + list(new_shape_1[-2:])
-            new_in_1 = broadcast_to(input_1, new_shape_1, final_shape_1, algorithms_1, next_tensors_1, self.manager)
-            updated_algorithms.extend(algorithms_1)
-            new_shape_1 = final_shape_1
+            algorithms_1, next_tensors_1 = [], []
+            algorithms_2, next_tensors_2 = [], []
 
-        new_in_2 = input_2
-        algorithms_2, next_tensors_2 = [], []
-        if output_shape[:-2] != new_shape_2[:-2] and not all(e == 1 for e in new_shape_2[:-2]):
-            final_shape_2 = output_shape[:-2] + list(new_shape_2[-2:])
-            new_in_2 = broadcast_to(input_2, new_shape_2, final_shape_2, algorithms_2, next_tensors_2, self.manager)
-            updated_algorithms.extend(algorithms_2)
-            new_shape_2 = final_shape_2
+            final_shape_1 = new_shape_1.copy()
+            final_shape_2 = new_shape_2.copy()
+            for i in range(len(final_shape_1)):
+                if final_shape_1[i] != final_shape_2[i]:
+                    if final_shape_1[i] == 1:
+                        final_shape_1[i] = final_shape_2[i]
+                    elif final_shape_2[i] == 1:
+                        final_shape_2[i] = final_shape_1[i]
+                    else:
+                        raise ValueError(f"MaxOp broadcasting failed at dim {i}: {new_shape_1} vs {new_shape_2}")
 
-        if len(new_shape_1) == 1:
-            size_x_1 = new_shape_1[0]
-            size_y_1 = 1
-            size_z_1 = 1
-            size_x_2 = new_shape_2[0]
-            size_y_2 = 1
-            size_z_2 = 1
-        elif len(new_shape_1) == 2:
-            size_x_1 = new_shape_1[0]
-            size_y_1 = new_shape_1[1]
-            size_z_1 = 1
-            size_x_2 = new_shape_2[0]
-            size_y_2 = new_shape_2[1]
-            size_z_2 = 1
-        else:
-            size_x_1 = np.prod(new_shape_1[:-2])
-            size_y_1 = new_shape_1[-2]
-            size_z_1 = new_shape_1[-1]
-            size_x_2 = np.prod(new_shape_2[:-2])
-            size_y_2 = new_shape_2[-2]
-            size_z_2 = new_shape_2[-1]
+            tensor_in_1 = broadcast_to(input_1, new_shape_1, final_shape_1, algorithms_1, next_tensors_1, self.manager)
+            tensor_in_2 = broadcast_to(input_2, new_shape_2, final_shape_2, algorithms_2, next_tensors_2, self.manager)
 
-        size = np.prod(output_shape)
-        tensor_out = self.manager.tensor(np.zeros(size, dtype=np.float32))
-        updated_tensors.append(tensor_out)
-        workgroup = (max(size_x_1, size_x_2), max(size_y_1, size_y_2), max(size_z_1, size_z_2))
-        updated_algorithms.append(self.manager.algorithm([new_in_1, new_in_2, tensor_out],
-                                                         self.compiled_shader,
-                                                         workgroup,
-                                                         [size_x_1, size_y_1, size_z_1, size_x_2, size_y_2, size_z_2],
-                                                         []))
+            updated_algorithms.extend(algorithms_1 + algorithms_2)
+            updated_tensors.extend(next_tensors_1 + next_tensors_2)
 
-        return [(tensor_out, output_shape)]
+            output_shape = final_shape_1
+
+            if len(output_shape) == 1:
+                size_x_1 = output_shape[0]
+                size_y_1 = 1
+                size_z_1 = 1
+                size_x_2 = output_shape[0]
+                size_y_2 = 1
+                size_z_2 = 1
+            elif len(output_shape) == 2:
+                size_x_1 = output_shape[0]
+                size_y_1 = output_shape[1]
+                size_z_1 = 1
+                size_x_2 = output_shape[0]
+                size_y_2 = output_shape[1]
+                size_z_2 = 1
+            else:
+                size_x_1 = np.prod(output_shape[:-2])
+                size_y_1 = output_shape[-2]
+                size_z_1 = output_shape[-1]
+                size_x_2 = np.prod(output_shape[:-2])
+                size_y_2 = output_shape[-2]
+                size_z_2 = output_shape[-1]
+
+            tensor_out = self.manager.tensor(np.zeros(np.prod(output_shape), dtype=np.float32))
+            updated_tensors.append(tensor_out)
+            workgroup = (max(size_x_1, size_x_2), max(size_y_1, size_y_2), max(size_z_1, size_z_2))
+            updated_algorithms.append(
+                self.manager.algorithm([tensor_in_1, tensor_in_2, tensor_out],
+                                       self.compiled_shader,
+                                       workgroup,
+                                       [size_x_1, size_y_1, size_z_1, size_x_2, size_y_2, size_z_2],
+                                       [])
+            )
+
+            return tensor_out, output_shape
+
+        cur = input_tensors[0]
+        for i in range(1, len(input_tensors)):
+            cur = _fuse_two([cur, input_tensors[i]])
+
+        return [cur]
