1423bd0fd508ef70d368349346304dd37e1493f0
jfdu <55056593+dududjf@users.noreply.github.com>
2025-09-02 00:14:31 +0800

    Add test cases for prelu and reducemean operators

---
+++ src/kp_onnx_test/kop_reducemean_test.py
@@ -0,0 +1,139 @@
+from kp import Manager
+import numpy as np
+import time
+from kp_onnx.kop_reducemean import ReduceMeanOp
+
+# Device
+device_id = 0
+mgr = Manager(device_id)
+print(mgr.get_device_properties())
+
+reduce_mean_op = ReduceMeanOp(mgr)
+x = np.random.random((255, 127, 63)).astype(np.float32)
+
+
+def handle_axes(axes):
+    if isinstance(axes, tuple):
+        if len(axes) == 0:
+            return None
+        return axes
+    if axes is None:
+        return None
+    if isinstance(axes, (int, tuple)):
+        return axes
+    if not isinstance(axes, np.ndarray):
+        raise TypeError(f"axes must be an array, not {type(axes)}.")
+    if len(axes.shape) == 0:
+        return int(axes)
+    if 0 in axes.shape:
+        return None
+    return tuple(axes.ravel().tolist())
+
+
+def np_mean(data, axes=None, keepdims=1, noop_with_empty_axes=0):
+    if axes is None and noop_with_empty_axes:
+        return data
+    keepdims = keepdims != 0  # type: ignore
+    axes = handle_axes(axes)
+    try:
+        res = np.mean(data, axis=axes, keepdims=keepdims, dtype=data.dtype)  # type: ignore
+        if not keepdims and not isinstance(res, np.ndarray):
+            # The runtime must return a numpy array of a single float.
+            res = np.array(res)
+    except TypeError as e:
+        raise TypeError(
+            f"Unable to reduce shape {data.shape!r} with axes={axes!r} and keepdims={keepdims}."
+        ) from e
+    return res
+
+
+# -------- Case 1: axis: None, keepdims: 1, noop_with_empty_axes: 0 --------
+print("Case 1: axis: None, keepdims: 1, noop_with_empty_axes: 0")
+start_time = time.time()
+np_out = np_mean(x)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x)[0]
+print(f"{reduce_mean_op}: ", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
+print("----")
+
+# -------- Case 2: axis: None, keepdims: 0, noop_with_empty_axes: 0 --------
+print("Case 2: axis: None, keepdims: 0, noop_with_empty_axes: 0")
+start_time = time.time()
+np_out = np_mean(x, keepdims=0)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x, None, 0)[0]
+print(f"{reduce_mean_op}: ", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
+print("----")
+
+# -------- Case 3: axes: None, keepdims: 1, noop_with_empty_axes: 1 --------
+print("Case 3: axes: None, keepdims: 1, noop_with_empty_axes: 1")
+start_time = time.time()
+np_out = np_mean(x, None, keepdims=1, noop_with_empty_axes=1)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x, None, 1, 1)[0]
+print(f"{reduce_mean_op}:", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
+print("----")
+
+# -------- Case 4: axes: None, keepdims: 0, noop_with_empty_axes: 1 --------
+print("Case 4: axes: None, keepdims: 0, noop_with_empty_axes: 1")
+start_time = time.time()
+np_out = np_mean(x, None, keepdims=0, noop_with_empty_axes=1)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x, None, 0, 1)[0]
+print(f"{reduce_mean_op}:", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
+print("----")
+
+# -------- Case 5: axes: [1,2], keepdims: 1, noop_with_empty_axes: 0 --------
+print("Case 5: axes: [1,2], keepdims: 1, noop_with_empty_axes: 0")
+axes = np.array([1, 2], dtype=np.int32)
+start_time = time.time()
+np_out = np_mean(x, axes)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x, axes)[0]
+print(f"{reduce_mean_op}: ", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
+print("----")
+
+# -------- Case 6: axes: 1, keepdims: 0, noop_with_empty_axes: 0 --------
+print("Case 6: axes: (1), keepdims: 0, noop_with_empty_axes: 0")
+axes = (1, )
+start_time = time.time()
+np_out = np_mean(x, axes, keepdims=0)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = reduce_mean_op.run(x, axes, 0)[0]
+print(f"{reduce_mean_op}: ", time.time() - start_time, "seconds")
+
+print("shape equal:", kp_out.shape == np_out.shape)
+print("Max error:", np.abs(np_out - kp_out).max())
+print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
