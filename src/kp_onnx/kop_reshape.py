95ea987f17e2898694a7624928ef0054f2bda265
fzc04 <3247348249@qq.com>
2025-09-09 12:47:41 +0800

    Update kop_reshape.py

--- src/kp_onnx/kop_reshape.py
+++ src/kp_onnx/kop_reshape.py
@@ -17,39 +17,24 @@ class ReshapeOp:
     def run(self, *inputs):
         assert len(inputs) >= 2, "ReshapeOp requires at least 2 inputs"
 
-        input_tensors = []
-        for inp in inputs:
-            numpy_in = inp.reshape(-1) if isinstance(inp, np.ndarray) else np.array(inp)
-            tensor = self.manager.tensor(numpy_in)
-            input_tensors.append((tensor, list(inp.shape) if isinstance(inp, np.ndarray) else []))
+        input_tensors = [(inputs[0], list(inputs[0].data.shape))]
 
         updated_algorithms, updated_tensors = [], []
-        output_tensor_and_shape = self.fuse(input_tensors, updated_algorithms, updated_tensors)
+        output_tensor_and_shape = self.fuse(input_tensors, list(inputs[1:]),
+                                            updated_algorithms, updated_tensors)
         tensor_out, output_shape = output_tensor_and_shape[0]
 
-        output = tensor_out.data().reshape(output_shape)
-
-        for tensor, _ in input_tensors:
-            del tensor
-        del updated_tensors
-
+        output = np.array(tensor_out.data).reshape(output_shape)
         return (output,)
 
-    def fuse(self, input_tensors: list[tuple[kp.Tensor, list[int]]],updated_algorithms: list[kp.Algorithm],
+    def fuse(self, input_tensors: list[tuple[kp.Tensor, list[int]]],shape_tensors: list[kp.Tensor],updated_algorithms: list[kp.Algorithm],
              updated_tensors: list[kp.Tensor]) -> list[tuple[kp.Tensor, list[int]]]:
-        assert len(input_tensors) >= 2, "ReshapeOp requires at least 2 inputs"
 
-        tensor_in = input_tensors[0][0]
-        tensor_shape = input_tensors[0][1]
-        input_data = tensor_in.data()
+        tensor_in, tensor_shape = input_tensors[0]
+        shape_data = shape_tensors[0].data
+        new_shape = [int(x) for x in np.array(shape_data)]
 
-        shape_data = input_tensors[1][0].data()
-        new_shape = [int(x) for x in shape_data]
-
-        if len(input_tensors) > 2:
-            allowzero = int(input_tensors[2][0].data())
-        else:
-            allowzero = 0  # 默认 0
+        allowzero = int(np.array(shape_tensors[1].data).item()) if len(shape_tensors) > 1 else 0
 
         minus_one_count = new_shape.count(-1)
         if minus_one_count == 1:
@@ -66,9 +51,4 @@ class ReshapeOp:
         assert np.prod(new_shape) == np.prod(tensor_shape), \
             f"Cannot reshape tensor of size {np.prod(tensor_shape)} into shape {new_shape}"
 
-        reshaped_data = input_data.reshape(new_shape)
-
-        tensor_out = self.manager.tensor(reshaped_data)
-        updated_tensors.append(tensor_out)
-
-        return [(tensor_out, new_shape)]
+        return [(tensor_in, new_shape)]
