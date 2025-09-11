288e270c24f07e197c1acdfd05a726621fd783a9
zzzxxxppp1 <zzzxxxppp1@yahoo.com>
2025-09-11 13:33:50 +0800

    Update argmin

--- src/kp_onnx/kop_argmin.py
+++ src/kp_onnx/kop_argmin.py
@@ -13,70 +13,51 @@ class ArgMinOp:
 
     def __init__(self, manager: kp.Manager):
         self.manager = manager
-        self.shader_first = compile_source("""
+        self.shader = compile_source("""
 #version 450
 layout(local_size_x=1, local_size_y=1) in;
 
 layout(binding=0) readonly buffer InBuf  { float in_buf[]; };
 layout(binding=1) writeonly buffer OutBuf{ int   out_buf[]; };
 
-layout(constant_id=0) const float AXIS_SIZE_F    = 0.0;
-layout(constant_id=1) const float STRIDE_AFTER_F = 0.0;
+layout(constant_id=0) const float axis_size_f    = 0.0;
+layout(constant_id=1) const float stride_after_f = 0.0;
+layout(constant_id=2) const float select_last_f  = 0.0;
 
 void main() {
-    uint AXIS_SIZE    = uint(AXIS_SIZE_F);
-    uint STRIDE_AFTER = uint(STRIDE_AFTER_F);
+    uint axis_size    = uint(axis_size_f);
+    uint stride_after = uint(stride_after_f);
+    uint select_last  = uint(select_last_f);
 
-    uint out_before = gl_GlobalInvocationID.x;  // ∈ [0, stride_before)
-    uint out_after  = gl_GlobalInvocationID.y;  // ∈ [0, stride_after)
+    uint out_before = gl_GlobalInvocationID.x;
+    uint out_after  = gl_GlobalInvocationID.y;
 
-    uint base = (out_before * AXIS_SIZE) * STRIDE_AFTER + out_after;
+    uint base = (out_before * axis_size) * stride_after + out_after;
 
     float min_val = in_buf[base];
     uint  min_idx = 0u;
-    base += STRIDE_AFTER;
-    for (uint i = 1u; i < AXIS_SIZE; ++i, base += STRIDE_AFTER) {
-        float v = in_buf[base];
-        if (v < min_val) {
-            min_val = v;
-            min_idx = i;
+    base += stride_after;
+
+    if (select_last == 1u) {
+        for (uint i = 1u; i < axis_size; ++i, base += stride_after) {
+            float v = in_buf[base];
+            if (v <= min_val) {
+                min_val = v;
+                min_idx = i;
+            }
+        }
+    } 
+    else {
+        for (uint i = 1u; i < axis_size; ++i, base += stride_after) {
+            float v = in_buf[base];
+            if (v < min_val) {
+                min_val = v;
+                min_idx = i;
+            }
         }
     }
-    out_buf[out_before * STRIDE_AFTER + out_after] = int(min_idx);
+    out_buf[out_before * stride_after + out_after] = int(min_idx);
 }
-""")
-        self.shader_last = compile_source("""
-#version 450
-layout(local_size_x=1, local_size_y=1) in;
-
-layout(binding=0) readonly buffer InBuf  { float in_buf[]; };
-layout(binding=1) writeonly buffer OutBuf{ int   out_buf[]; };
-
-layout(constant_id=0) const float AXIS_SIZE_F    = 0.0;
-layout(constant_id=1) const float STRIDE_AFTER_F = 0.0;
-
-void main() {
-    uint AXIS_SIZE    = uint(AXIS_SIZE_F);
-    uint STRIDE_AFTER = uint(STRIDE_AFTER_F);
-
-    uint out_before = gl_GlobalInvocationID.x;  // ∈ [0, stride_before)
-    uint out_after  = gl_GlobalInvocationID.y;  // ∈ [0, stride_after)
-
-    uint base = (out_before * AXIS_SIZE) * STRIDE_AFTER + out_after;
-
-    float min_val = in_buf[base];
-    uint  min_idx = 0u;
-    base += STRIDE_AFTER;
-    for (uint i = 1u; i < AXIS_SIZE; ++i, base += STRIDE_AFTER) {
-        float v = in_buf[base];
-        if (v <= min_val) {
-            min_val = v;
-            min_idx = i;
-        }
-    }
-    out_buf[out_before * STRIDE_AFTER + out_after] = int(min_idx);
-}
-
 """)
 
     def __repr__(self):
@@ -131,7 +112,6 @@ void main() {
         stride_before = int(np.prod(shape[:axis])) if axis > 0 else 1
         stride_after  = int(np.prod(shape[axis+1:])) if axis < rank - 1 else 1
 
-        # ---- 输出形状 ----
         out_shape = list(shape)
         out_shape[axis:axis + 1] = [1] if keepdims else []
 
@@ -142,13 +122,13 @@ void main() {
         tensor_out = self.manager.tensor_t(np.zeros(output_size, dtype=np.int32), tensor_type=kp.TensorTypes.device)
         updated_tensors.append(tensor_out)
         workgroup = (stride_before, stride_after, 1)
-        shader = self.shader_last if select_last_index else self.shader_first
+        shader = self.shader
         updated_algorithms.append(
             self.manager.algorithm(
                 [tensor_in, tensor_out],
                 shader,
                 workgroup,
-                [axis_size, stride_after],
+                [axis_size, stride_after, select_last_index],
                 []
             )
         )
