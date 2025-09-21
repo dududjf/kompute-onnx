925fadeb777037885776c5a0b036c5d87c97cc06
fzc04 <3247348249@qq.com>
2025-09-21 19:14:32 +0800

    Update kop_reduce_prod_test.py

--- src/kp_onnx_test/kop_reduce_prod_test.py
+++ src/kp_onnx_test/kop_reduce_prod_test.py
@@ -9,8 +9,7 @@ mgr = Manager(device_id)
 print(mgr.get_device_properties())
 
 reduce_prod_op = ReduceProdOp(mgr)
-x = np.random.random((255, 127, 63)).astype(np.float32)
-
+x = np.random.random((255, 1024, 1024)).astype(np.float32)
 
 def handle_axes(axes):
     if isinstance(axes, tuple):
@@ -29,11 +28,9 @@ def handle_axes(axes):
         return None
     return tuple(axes.ravel().tolist())
 
-
-def np_prod(data, axes=None, keepdims=1, noop_with_empty_axes=0):
+def np_prod(data, axes=None, keepdims=True, noop_with_empty_axes=False):
     if axes is None and noop_with_empty_axes:
         return data
-    keepdims = keepdims != 0
     axes = handle_axes(axes)
     try:
         res = np.prod(data, axis=axes, keepdims=keepdims, dtype=data.dtype)
@@ -46,13 +43,14 @@ def np_prod(data, axes=None, keepdims=1, noop_with_empty_axes=0):
     return res
 
 
-# -------- Case 1: axis: None, keepdims: 1, noop_with_empty_axes: 0 --------
-print("Case 1: axis: None, keepdims: 1, noop_with_empty_axes: 0")
+print("Case 1: keepdims=True, noop_with_empty_axes=False, axes=None")
 start_time = time.time()
-np_out = np_prod(x)
+np_out = np_prod(x, keepdims=True, noop_with_empty_axes=False)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
+reduce_prod_op.keepdims = True
+reduce_prod_op.noop_with_empty_axes = False
 kp_out = reduce_prod_op.run(x)[0]
 print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
@@ -61,14 +59,16 @@ print("Max error:", np.abs(np_out - kp_out).max())
 print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
 print("----")
 
-# -------- Case 2: axis: None, keepdims: 0, noop_with_empty_axes: 0 --------
-print("Case 2: axis: None, keepdims: 0, noop_with_empty_axes: 0")
+
+print("Case 2: keepdims=False, noop_with_empty_axes=False, axes=None")
 start_time = time.time()
-np_out = np_prod(x, keepdims=0)
+np_out = np_prod(x, keepdims=False, noop_with_empty_axes=False)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
-kp_out = reduce_prod_op.run(x, None, 0)[0]
+reduce_prod_op.keepdims = False
+reduce_prod_op.noop_with_empty_axes = False
+kp_out = reduce_prod_op.run(x)[0]
 print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
 print("shape equal:", kp_out.shape == np_out.shape)
@@ -76,44 +76,50 @@ print("Max error:", np.abs(np_out - kp_out).max())
 print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
 print("----")
 
-# -------- Case 3: axes: None, keepdims: 1, noop_with_empty_axes: 1 --------
-print("Case 3: axes: None, keepdims: 1, noop_with_empty_axes: 1")
+
+print("Case 3: keepdims=True, noop_with_empty_axes=True, axes=None")
 start_time = time.time()
-np_out = np_prod(x, None, keepdims=1, noop_with_empty_axes=1)
+np_out = np_prod(x, keepdims=True, noop_with_empty_axes=True)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
-kp_out = reduce_prod_op.run(x, None, 1, 1)[0]
-print(f"{reduce_prod_op}:", time.time() - start_time, "seconds")
+reduce_prod_op.keepdims = True
+reduce_prod_op.noop_with_empty_axes = True
+kp_out = reduce_prod_op.run(x, None)[0]
+print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
 print("shape equal:", kp_out.shape == np_out.shape)
 print("Max error:", np.abs(np_out - kp_out).max())
 print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
 print("----")
 
-# -------- Case 4: axes: None, keepdims: 0, noop_with_empty_axes: 1 --------
-print("Case 4: axes: None, keepdims: 0, noop_with_empty_axes: 1")
+
+print("Case 4: keepdims=False, noop_with_empty_axes=True, axes=None")
 start_time = time.time()
-np_out = np_prod(x, None, keepdims=0, noop_with_empty_axes=1)
+np_out = np_prod(x, keepdims=False, noop_with_empty_axes=True)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
-kp_out = reduce_prod_op.run(x, None, 0, 1)[0]
-print(f"{reduce_prod_op}:", time.time() - start_time, "seconds")
+reduce_prod_op.keepdims = False
+reduce_prod_op.noop_with_empty_axes = True
+kp_out = reduce_prod_op.run(x, None)[0]
+print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
 print("shape equal:", kp_out.shape == np_out.shape)
 print("Max error:", np.abs(np_out - kp_out).max())
 print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
 print("----")
 
-# -------- Case 5: axes: [1,2], keepdims: 1, noop_with_empty_axes: 0 --------
-print("Case 5: axes: [1,2], keepdims: 1, noop_with_empty_axes: 0")
-axes = np.array([1, 2], dtype=np.int32)
+
+print("Case 5: keepdims=True, axes=[0,2]")
+axes = np.array([0, 2], dtype=np.int32)
 start_time = time.time()
-np_out = np_prod(x, axes)
+np_out = np_prod(x, axes, keepdims=True, noop_with_empty_axes=False)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
+reduce_prod_op.keepdims = True
+reduce_prod_op.noop_with_empty_axes = False
 kp_out = reduce_prod_op.run(x, axes)[0]
 print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
@@ -122,15 +128,17 @@ print("Max error:", np.abs(np_out - kp_out).max())
 print(np.allclose(np_out, kp_out, rtol=1e-4, atol=1e-4))
 print("----")
 
-# -------- Case 6: axes: 1, keepdims: 0, noop_with_empty_axes: 0 --------
-print("Case 6: axes: (1), keepdims: 0, noop_with_empty_axes: 0")
-axes = (1,)
+
+print("Case 6: keepdims=False, axes=(0,)")
+axes = (0,)
 start_time = time.time()
-np_out = np_prod(x, axes, keepdims=0)
+np_out = np_prod(x, axes, keepdims=False, noop_with_empty_axes=False)
 print("Numpy:", time.time() - start_time, "seconds")
 
 start_time = time.time()
-kp_out = reduce_prod_op.run(x, axes, 0)[0]
+reduce_prod_op.keepdims = False
+reduce_prod_op.noop_with_empty_axes = False
+kp_out = reduce_prod_op.run(x, axes)[0]
 print(f"{reduce_prod_op}: ", time.time() - start_time, "seconds")
 
 print("shape equal:", kp_out.shape == np_out.shape)
