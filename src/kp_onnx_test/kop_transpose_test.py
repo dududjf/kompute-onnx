65bfdb8804595f91c113ddd674eab220eeaf7817
Kazuya <zzzxxxppp1@yahoo.com>
2025-09-23 19:42:51 +0800

    Update transpose_test

--- src/kp_onnx_test/kop_transpose_test.py
+++ src/kp_onnx_test/kop_transpose_test.py
@@ -1,7 +1,7 @@
 from kp import Manager
 import numpy as np
 import time
-from kp_onnx.kop_transpose2 import TransposeOp
+from kp_onnx.kop_transpose import TransposeOp
 
 device_id = 0
 mgr = Manager(device_id)
