c02fe33d09c991a565fdfa1412d00e86123c2a48
fzc04 <3247348249@qq.com>
2025-09-27 11:22:57 +0800

    Update kop_reduce_prod.py

--- src/kp_onnx/kop_reduce_prod.py
+++ src/kp_onnx/kop_reduce_prod.py
@@ -55,12 +55,13 @@ void main()
         output_tensor_and_shape = self.fuse(input_tensors, updated_algorithms, updated_tensors)
         tensor_out, output_shape = output_tensor_and_shape[0]
 
-        seq = self.manager.sequence()
-        seq.record(kp.OpTensorSyncDevice([input_tensors[0][0]]))
-        for alg in updated_algorithms:
-            seq.record(kp.OpAlgoDispatch(alg))
-        seq.record(kp.OpTensorSyncLocal([tensor_out]))
-        seq.eval()
+        if updated_algorithms:
+            seq = self.manager.sequence()
+            seq.record(kp.OpTensorSyncDevice([input_tensors[0][0]]))
+            for alg in updated_algorithms:
+                seq.record(kp.OpAlgoDispatch(alg))
+            seq.record(kp.OpTensorSyncLocal([tensor_out]))
+            seq.eval()
 
         output = tensor_out.data().reshape(output_shape)
 
