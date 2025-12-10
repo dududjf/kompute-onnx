eebd71c6642e0dd6d49270abbd3c3f67198b2b97
fzc04 <3247348249@qq.com>
2025-12-10 12:47:41 +0800

    Update kop_stft.py

--- src/kp_onnx/kop_stft.py
+++ src/kp_onnx/kop_stft.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import kp
 import numpy as np
 from .shader_utils import compile_source
@@ -11,14 +12,14 @@ class STFTOp:
 #version 450
 layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
-layout (binding = 0) buffer buf_in_signal { float in_signal[]; };
-layout (binding = 1) buffer buf_in_window { float in_window[]; };
-layout (binding = 2) buffer buf_out_tensor { float out_tensor[]; };
+layout (binding = 0) buffer buf_in_x       { float in_x[]; };
+layout (binding = 1) buffer buf_in_window  { float in_window[]; };
+layout (binding = 2) buffer buf_out_result { float out_result[]; };
 
-layout (constant_id = 0) const float signal_len_f = 0;
-layout (constant_id = 1) const float frame_step_f = 0;
-layout (constant_id = 2) const float frame_len_f = 0;
-layout (constant_id = 3) const float fft_len_f = 0;
+layout (constant_id = 0) const float x_len_f = 0;
+layout (constant_id = 1) const float hop_length_f = 0;
+layout (constant_id = 2) const float frame_length_f = 0;
+layout (constant_id = 3) const float fft_length_f = 0;
 layout (constant_id = 4) const float n_frames_f = 0;
 layout (constant_id = 5) const float n_freqs_f = 0;
 layout (constant_id = 6) const float use_window_f = 0;
@@ -27,56 +28,55 @@ layout (constant_id = 6) const float use_window_f = 0;
 
 void main()
 {
-    uint frame_idx = gl_GlobalInvocationID.x;
-    uint freq_idx = gl_GlobalInvocationID.y;
+    uint fs = gl_GlobalInvocationID.x;
+    uint k  = gl_GlobalInvocationID.y;
     uint batch_idx = gl_GlobalInvocationID.z;
 
     uint n_frames = uint(n_frames_f);
-    uint n_freqs = uint(n_freqs_f);
+    uint n_freqs  = uint(n_freqs_f);
 
-    if (frame_idx >= n_frames || freq_idx >= n_freqs) return;
-
-    uint signal_len = uint(signal_len_f);
-    uint frame_step = uint(frame_step_f);
-    uint frame_len = uint(frame_len_f);
-    uint fft_len = uint(fft_len_f);
+    uint x_len = uint(x_len_f);
+    uint hop_length = uint(hop_length_f);
+    uint frame_length = uint(frame_length_f);
+    uint fft_length = uint(fft_length_f);
     uint use_window = uint(use_window_f);
 
-    uint stride_freq = 2;
+    uint stride_freq = 2; 
     uint stride_frame = n_freqs * stride_freq;
     uint stride_batch = n_frames * stride_frame;
 
-    uint out_idx = (batch_idx * stride_batch) + (frame_idx * stride_frame) + (freq_idx * stride_freq);
-    uint signal_start = (batch_idx * signal_len) + (frame_idx * frame_step);
+    uint out_idx = (batch_idx * stride_batch) + (fs * stride_frame) + (k * stride_freq);
+
+    uint begin = (batch_idx * x_len) + (fs * hop_length);
 
     float sum_r = 0.0;
     float sum_i = 0.0;
 
-    uint phase_acc = 0; 
+    uint phase_acc = 0;
 
-    for (uint t = 0; t < frame_len; ++t) {
+    for (uint n = 0; n < frame_length; ++n) {
         float val = 0.0;
-        uint current_signal_idx = signal_start + t;
+        uint current_idx = begin + n;
 
-        if (current_signal_idx < (batch_idx + 1) * signal_len) {
-            val = in_signal[current_signal_idx];
+        if (current_idx < (batch_idx + 1) * x_len) {
+            val = in_x[current_idx];
         }
 
         if (use_window == 1) {
-            val *= in_window[t];
+            val *= in_window[n];
         }
 
-        uint phase_idx = phase_acc % fft_len;
-        float angle = -2.0 * PI * float(phase_idx) / float(fft_len);
+        uint phase_idx = phase_acc % fft_length;
+        float angle = -2.0 * PI * float(phase_idx) / float(fft_length);
 
         sum_r += val * cos(angle);
         sum_i += val * sin(angle);
 
-        phase_acc += freq_idx;
+        phase_acc += k;
     }
 
-    out_tensor[out_idx] = sum_r;
-    out_tensor[out_idx + 1] = sum_i;
+    out_result[out_idx]     = sum_r;
+    out_result[out_idx + 1] = sum_i;
 }
 ''')
 
@@ -113,12 +113,13 @@ void main()
         del updated_tensors
         return [output]
 
-    def fuse(self, input_tensors: list, updated_algorithms: list[kp.Algorithm],
+    def fuse(self, input_tensors: list[tuple[kp.Tensor, list[int]]], updated_algorithms: list[kp.Algorithm],
              updated_tensors: list[kp.Tensor]) -> list[tuple[kp.Tensor, list[int]]]:
-        tensor_signal = input_tensors[0][0]
-        shape_signal = input_tensors[0][1]
-        frame_step_data = input_tensors[1][0].data()
-        frame_step = int(frame_step_data[0])
+
+        tensor_x = input_tensors[0][0]
+        shape_x = input_tensors[0][1]
+
+        hop_length = int(input_tensors[1][0].data()[0])
 
         tensor_window = None
         shape_window = []
@@ -126,25 +127,22 @@ void main()
             tensor_window = input_tensors[2][0]
             shape_window = input_tensors[2][1]
 
-        frame_len_input = None
+        frame_length_input = None
         if len(input_tensors) > 3 and input_tensors[3] is not None:
-            frame_len_data = input_tensors[3][0].data()
-            frame_len_input = int(frame_len_data[0])
+            frame_length_input = int(input_tensors[3][0].data()[0])
 
-        signal_len = shape_signal[-1]
+        x_len = shape_x[-1]
 
-        if frame_len_input is not None:
-            frame_length = frame_len_input
+        if frame_length_input is not None:
+            frame_length = frame_length_input
         elif tensor_window is not None:
             frame_length = shape_window[0]
         else:
-            frame_length = signal_len
+            frame_length = x_len
 
         fft_length = frame_length
 
-        assert frame_step > 0, "frame_step must be > 0"
-
-        n_frames = 1 + (signal_len - frame_length) // frame_step
+        n_frames = 1 + (x_len - frame_length) // hop_length
         if n_frames < 0:
             n_frames = 0
 
@@ -153,24 +151,21 @@ void main()
         else:
             n_freqs = fft_length
 
-        if len(shape_signal) > 1:
-            batch_size = np.prod(shape_signal[:-1])
-        else:
-            batch_size = 1
+        batch_size = int(np.prod(shape_x[:-1])) if len(shape_x) > 1 else 1
 
-        output_shape = shape_signal[:-1] + [n_frames, n_freqs, 2]
-        total_elements = np.prod(output_shape)
+        output_shape = shape_x[:-1] + [n_frames, n_freqs, 2]
+        total_elements = int(np.prod(output_shape))
 
         tensor_out = self.manager.tensor(np.zeros(total_elements, dtype=np.float32))
         updated_tensors.append(tensor_out)
 
         if total_elements > 0:
             use_window_val = 1.0 if tensor_window is not None else 0.0
-            bind_window_tensor = tensor_window if tensor_window is not None else tensor_signal
+            bind_window_tensor = tensor_window if tensor_window is not None else tensor_x
 
             params = [
-                signal_len,
-                frame_step,
+                x_len,
+                hop_length,
                 frame_length,
                 fft_length,
                 n_frames,
@@ -181,7 +176,7 @@ void main()
             workgroup = (n_frames, n_freqs, batch_size)
 
             updated_algorithms.append(self.manager.algorithm(
-                [tensor_signal, bind_window_tensor, tensor_out],
+                [tensor_x, bind_window_tensor, tensor_out],
                 self.compiled_shader,
                 workgroup,
                 params,
